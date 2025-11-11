89a1482340358ddd10129eedc7930921f17c7ba2
Merveil20 <djuidjemerveil20@gmail.com>
2025-11-10 20:31:39 -0400

    Depot initial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '/',
+  },
+  {
+    path: '/education',
+  },
+  {
+    path: '/skill',
+  },
+  {
+    path: '/projects',
+  },
+  {
+    path: '/projection',
+  },
+  {
+    path: '/contact',
+  },
+];
