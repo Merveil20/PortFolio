ff1a69abc5bd1e7459f57bf6c9020f4d020152b6
Merveil20 <djuidjemerveil20@gmail.com>
2025-11-11 21:09:19 -0400

    Structuration du projet et construction du header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,22 +1,3 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [
-  {
-    path: '/',
-  },
-  {
-    path: '/education',
-  },
-  {
-    path: '/skill',
-  },
-  {
-    path: '/projects',
-  },
-  {
-    path: '/projection',
-  },
-  {
-    path: '/contact',
-  },
-];
+export const routes: Routes = [];
