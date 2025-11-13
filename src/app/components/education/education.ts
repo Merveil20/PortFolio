879760ad377ab372e4746ae26e89c6db5836e4b3
Merveil20 <djuidjemerveil20@gmail.com>
2025-11-12 21:16:44 -0400

    Finalisation du home

---
+++ src/app/components/education/education.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-education',
+  imports: [],
+  templateUrl: './education.html',
+  styleUrl: './education.css',
+})
+export default class Education {}
